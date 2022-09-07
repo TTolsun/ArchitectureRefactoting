747ea9999fa9863b7d7101181a589627d5facee2
TTolsun <baboess@gmail.com>
2022-09-07 15:15:23 +0900

    Log Method
    Signed-off-by: TTolsun <baboess@gmail.com>

--- VideoRantal/Customer.java
+++ VideoRantal/Customer.java
@@ -38,47 +38,19 @@ public class Customer {
 		int totalPoint = 0;
 
 		for (Rental each : rentals) {
-			double eachCharge = 0;
-			int eachPoint = 0 ;
-			int daysRented = 0;
-
-			if (each.getStatus() == 1) { // returned VideoRantal.Video
-				long diff = each.getReturnDate().getTime() - each.getRentDate().getTime();
-				daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1;
-			} else { // not yet returned
-				long diff = new Date().getTime() - each.getRentDate().getTime();
-				daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1;
-			}
-
-			switch (each.getVideo().getPriceCode()) {
-			case Video.REGULAR:
-				eachCharge += 2;
-				if (daysRented > 2)
-					eachCharge += (daysRented - 2) * 1.5;
-				break;
-			case Video.NEW_RELEASE:
-				eachCharge = daysRented * 3;
-				break;
-			}
-
-			eachPoint++;
-
-			if ((each.getVideo().getPriceCode() == Video.NEW_RELEASE) )
-				eachPoint++;
-
-			if ( daysRented > each.getDaysRentedLimit() )
-				eachPoint -= Math.min(eachPoint, each.getVideo().getLateReturnPointPenalty()) ;
+			int daysRented = getDaysRental(each.getStatus() == 1
+					? each.getReturnDate().getTime() - each.getRentDate().getTime()
+					: new Date().getTime() - each.getRentDate().getTime());
+			double eachCharge = getEachCharge(each, daysRented);
+			int eachPoint = getEachPoint(each, daysRented);
 
 			result += "\t" + each.getVideo().getTitle() + "\tDays rented: " + daysRented + "\tCharge: " + eachCharge
 					+ "\tPoint: " + eachPoint + "\n";
-
 			totalCharge += eachCharge;
-
 			totalPoint += eachPoint ;
 		}
 
 		result += "Total charge: " + totalCharge + "\tTotal Point:" + totalPoint + "\n";
-
 
 		if ( totalPoint >= 10 ) {
 			System.out.println("Congrat! You earned one free coupon");
@@ -87,5 +59,34 @@ public class Customer {
 			System.out.println("Congrat! You earned two free coupon");
 		}
 		return result ;
+	}
+
+	private static int getEachPoint(Rental each, int daysRented) {
+		int eachPoint = 1;
+
+		if ((each.getVideo().getPriceCode() == Video.NEW_RELEASE) )
+			eachPoint++;
+		if ( daysRented > each.getDaysRentedLimit() )
+			eachPoint -= Math.min(eachPoint, each.getVideo().getLateReturnPointPenalty()) ;
+		return eachPoint;
+	}
+
+	private static double getEachCharge(Rental each, int daysRented) {
+		double eachCharge = 0;
+		switch (each.getVideo().getPriceCode()) {
+		case Video.REGULAR:
+			eachCharge += 2;
+			if (daysRented > 2)
+				eachCharge += (daysRented - 2) * 1.5;
+			break;
+		case Video.NEW_RELEASE:
+			eachCharge = daysRented * 3;
+			break;
+		}
+		return eachCharge;
+	}
+
+	private static int getDaysRental(long diff) {
+		return (int) (diff / (1000 * 60 * 60 * 24)) + 1;
 	}
 }
